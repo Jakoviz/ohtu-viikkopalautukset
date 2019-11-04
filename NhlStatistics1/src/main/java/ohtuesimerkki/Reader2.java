55e968a6887c8748a568e00cc55cfdea076c158c
Jaakko Paavola <jakoviz@gmail.com>
2019-11-04 23:45:23 +0200

    teht 14

---
+++ NhlStatistics1/src/main/java/ohtuesimerkki/Reader2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.List;
+
+/**
+ *
+ * @author jaakkpaa
+ */
+public interface Reader2 {
+
+	List<Player> getPlayers();
+	
+}
