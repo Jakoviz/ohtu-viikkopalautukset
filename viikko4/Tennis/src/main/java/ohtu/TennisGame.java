4ee4f3e25038ac4836ca96a5175ae975d0fe2ad7
Jaakko Paavola <jaakko.paavola@helsinki.fi>
2019-11-23 00:32:54 +0200

    Uusi yritys

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -4,6 +4,7 @@ public class TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
+	private String score = "";
     private String player1Name;
     private String player2Name;
 
@@ -20,61 +21,46 @@ public class TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+        if (m_score1>=4 || m_score2>=4)
+			numericToPhrasalWhenOverThreePoints();
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+			numericToPhrasalWhenThreePointsOrUnder();
         return score;
     }
+
+	private void numericToPhrasalWhenOverThreePoints() {
+		int minusResult = m_score1-m_score2;
+		if (minusResult == 0) score = "Deuce";
+		else if (minusResult==1) score ="Advantage player1";
+		else if (minusResult ==-1) score ="Advantage player2";
+		else if (minusResult>=2) score = "Win for player1";
+		else score ="Win for player2";
+	}
+
+	private void numericToPhrasalWhenThreePointsOrUnder() {
+		score = phrasalExpressionWhenGameNotTied(1, m_score1, "");
+		if (m_score1 == m_score2) 
+			score += "-All";
+		else
+			score = phrasalExpressionWhenGameNotTied(2, m_score2, score + "-");
+	}
+
+	private String phrasalExpressionWhenGameNotTied(int playerNumber, int scoreNumeric, String scorePhrasal) {
+		switch(scoreNumeric)
+		{
+			case 0:
+				scorePhrasal+="Love";
+				break;
+			case 1:
+				scorePhrasal+="Fifteen";
+				break;
+			case 2:
+				scorePhrasal+="Thirty";
+				break;
+			case 3:
+				scorePhrasal+="Forty";
+				break;
+		}
+		return scorePhrasal;
+	}
 }
